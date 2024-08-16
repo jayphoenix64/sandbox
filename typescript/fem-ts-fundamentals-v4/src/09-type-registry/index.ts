7440855fe15dcc80e190b2cf97ba968971c0cd59
jayphoenix64 <phoenix.janus@gmail.com>
2024-08-16 16:22:24 +0200

    recompile index from lesson 9

--- typescript/fem-ts-fundamentals-v4/src/09-type-registry/index.ts
+++ typescript/fem-ts-fundamentals-v4/src/09-type-registry/index.ts
@@ -3,6 +3,4 @@ import { fetchRecord } from './lib/registry';
 const book = fetchRecord('book', 'bk_123');
 const magazine = fetchRecord('magazine', 'bk_123');
 
-console.log(book)
-console.log(magazine)
-console.log('all done.')
+console.log('all done.');
