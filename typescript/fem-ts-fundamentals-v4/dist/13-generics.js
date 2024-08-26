a2c516a638d6ed9bfba9dcb9e64cd5f4b87595ea
jayphoenix64 <phoenix.janus@gmail.com>
2024-08-26 08:58:39 +0200

    add lesson 13

--- typescript/fem-ts-fundamentals-v4/dist/13-generics.js
+++ typescript/fem-ts-fundamentals-v4/dist/13-generics.js
@@ -21,43 +21,33 @@ const phoneDict = {
     },
     /*... and so on */
 };
-/*
-// interface PhoneInfo {
-//   customerId: string
-//   areaCode: string
-//   num: string
-// }
-
-// function listToDict(
-//   list: PhoneInfo[], // take the list as an argument
-//   idGen: (arg: PhoneInfo) => string, // a callback to get Ids
-// ): { [k: string]: PhoneInfo } {}
-
-/*
+function listToDict(list, // take the list as an argument
+idGen // a callback to get Ids
+) {
+    const dict = {};
+    list.forEach((element) => {
+        const dictKey = idGen(element);
+        dict[dictKey] = element; // store element under key
+    });
+    return dict;
+}
 //? function body
 // // create an empty dictionary
 // const dict: { [k: string]: PhoneInfo } = {}
-
 // // Loop through the array
 // list.forEach((element) => {
 //   const dictKey = idGen(element)
 //   dict[dictKey] = element // store element under key
 // })
-
-// // return the dictionary
-// const result = listToDict(phoneList, (item) => item.customerId)
-// console.log(result)
-/*
+// return the dictionary
+const result = listToDict(phoneList, (item) => item.customerId);
+console.log(result);
 //? An attempt to generalize the above function to work with any type of list
-
 // function listToDict(
 //   list: PhoneInfo[], // take the list as an argument
 //   idGen: (arg: PhoneInfo) => string, // a callback to get Ids
 // ): { [k: string]: any }
-
 //* Defining a type parameter
-
-/*
 // function listToDict<T>(
 //   list: T[],
 //   idGen: (arg: T) => string,
@@ -65,35 +55,37 @@ const phoneDict = {
 //   const dict: { [k: string]: T } = {}
 //   return dict
 // }
-
-// function wrapInArray<T>(arg: T): [T] {
-//   return [arg]
-// }
-// wrapInArray(3)
-// //   ^?
-// wrapInArray(new Date())
-// //   ^?
-// wrapInArray(new RegExp("/s/"))
-
-/*
+function wrapInArray(arg) {
+    return [arg];
+}
+wrapInArray(3);
+//   ^ number
+wrapInArray(new Date());
+//   ^ Date
+wrapInArray(new RegExp('/s/'));
+//   ^ RegExp
 //? Let's try it!
-// listToDict(
-//   [
-//     new Date('10-01-2021'),
-//     new Date('03-14-2021'),
-//     new Date('06-03-2021'),
-//     new Date('09-30-2021'),
-//     new Date('02-17-2021'),
-//     new Date('05-21-2021'),
-//   ],
-//   (arg) => arg.toISOString(),
-// )
-
+listToDict([
+    new Date('10-01-2021'),
+    new Date('03-14-2021'),
+    new Date('06-03-2021'),
+    new Date('09-30-2021'),
+    new Date('02-17-2021'),
+    new Date('05-21-2021'),
+], (arg) => arg.toISOString());
 //* Best practices
+function returnAs(arg) {
+    return arg; //! an `any` that will _seem_ like a `T`
+} // may as well just cast
+const foo = returnAs(43);
+function makeTuple(arg1, arg2) {
+    return [arg1, arg2];
+}
+const first = makeTuple(1, 2);
+const second = makeTuple('foo', new Date());
+console.log(first, second);
 /*
-// function returnAs<T>(arg: any): T {
-//     return arg //! an `any` that will _seem_ like a `T`
-// } // may as well just cast
-
-/**/
+ * always use generics more than just in function signature because type might get lost
+ */
+console.log('all done.');
 exports.default = {};
--- typescript/fem-ts-fundamentals-v4/src/13-generics.ts
+++ typescript/fem-ts-fundamentals-v4/src/13-generics.ts
@@ -6,7 +6,7 @@ const phoneList = [
   { customerId: '0003', areaCode: '192', num: '012-7190' },
   { customerId: '0005', areaCode: '402', num: '652-5782' },
   { customerId: '0004', areaCode: '301', num: '184-8501' },
-]
+];
 const phoneDict = {
   '0001': {
     customerId: '0001',
@@ -19,20 +19,26 @@ const phoneDict = {
     num: '142-3626',
   },
   /*... and so on */
+};
+
+interface PhoneInfo {
+  customerId: string;
+  areaCode: string;
+  num: string;
 }
-/*
-// interface PhoneInfo {
-//   customerId: string
-//   areaCode: string
-//   num: string
-// }
 
-// function listToDict(
-//   list: PhoneInfo[], // take the list as an argument
-//   idGen: (arg: PhoneInfo) => string, // a callback to get Ids
-// ): { [k: string]: PhoneInfo } {}
+function listToDict<T>(
+  list: T[], // take the list as an argument
+  idGen: (arg: T) => string // a callback to get Ids
+): { [k: string]: T } {
+  const dict: { [k: string]: T } = {};
+  list.forEach((element) => {
+    const dictKey = idGen(element);
+    dict[dictKey] = element; // store element under key
+  });
+  return dict;
+}
 
-/*
 //? function body
 // // create an empty dictionary
 // const dict: { [k: string]: PhoneInfo } = {}
@@ -43,10 +49,10 @@ const phoneDict = {
 //   dict[dictKey] = element // store element under key
 // })
 
-// // return the dictionary
-// const result = listToDict(phoneList, (item) => item.customerId)
-// console.log(result)
-/*
+// return the dictionary
+const result = listToDict(phoneList, (item) => item.customerId);
+console.log(result);
+
 //? An attempt to generalize the above function to work with any type of list
 
 // function listToDict(
@@ -56,7 +62,6 @@ const phoneDict = {
 
 //* Defining a type parameter
 
-/*
 // function listToDict<T>(
 //   list: T[],
 //   idGen: (arg: T) => string,
@@ -65,34 +70,49 @@ const phoneDict = {
 //   return dict
 // }
 
-// function wrapInArray<T>(arg: T): [T] {
-//   return [arg]
-// }
-// wrapInArray(3)
-// //   ^?
-// wrapInArray(new Date())
-// //   ^?
-// wrapInArray(new RegExp("/s/"))
+function wrapInArray<T>(arg: T): [T] {
+  return [arg];
+}
+wrapInArray(3);
+//   ^ number
+wrapInArray(new Date());
+//   ^ Date
+wrapInArray(new RegExp('/s/'));
+//   ^ RegExp
 
-/*
 //? Let's try it!
-// listToDict(
-//   [
-//     new Date('10-01-2021'),
-//     new Date('03-14-2021'),
-//     new Date('06-03-2021'),
-//     new Date('09-30-2021'),
-//     new Date('02-17-2021'),
-//     new Date('05-21-2021'),
-//   ],
-//   (arg) => arg.toISOString(),
-// )
+listToDict(
+  [
+    new Date('10-01-2021'),
+    new Date('03-14-2021'),
+    new Date('06-03-2021'),
+    new Date('09-30-2021'),
+    new Date('02-17-2021'),
+    new Date('05-21-2021'),
+  ],
+  (arg) => arg.toISOString()
+);
 
 //* Best practices
-/*
-// function returnAs<T>(arg: any): T {
-//     return arg //! an `any` that will _seem_ like a `T`
-// } // may as well just cast
 
-/**/
-export default {}
+function returnAs<T>(arg: any): T {
+  return arg; //! an `any` that will _seem_ like a `T`
+} // may as well just cast
+const foo = returnAs<number>(43);
+
+function makeTuple<T, U>(arg1: T, arg2: U): [T, U] {
+  return [arg1, arg2];
+}
+
+const first = makeTuple(1, 2);
+const second = makeTuple('foo', new Date());
+
+console.log(first, second)
+
+/*
+ * always use generics more than just in function signature because type might get lost
+ */
+
+console.log('all done.');
+
+export default {};
