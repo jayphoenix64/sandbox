2833202b59e26f285795d5b2ffe47375d7b80454
jayphoenix64 <phoenix.janus@gmail.com>
2024-08-26 09:21:45 +0200

    add notes for lesson 1

---
+++ typescript/fem-ts-intermediate-v2/dist/01-welcome.js
@@ -0,0 +1,7 @@
+/**
+ * just for reference:
+ * Volta https://volta.sh/
+ *
+ * the typescript version used in the course was 5.3.0-beta
+ * i used 5.4.3 since it was the latest version and no errors occured in the fundamentals course
+ */
