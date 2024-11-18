b7053ab3f8cc23adbf00680cec807468bf315be9
jayphoenix64 <phoenix.janus@gmail.com>
2024-11-18 13:57:08 +0100

    finish lesson 7

--- typescript/fem-ts-intermediate-v2/src/07-conditional-types.ts
+++ typescript/fem-ts-intermediate-v2/src/07-conditional-types.ts
@@ -1,70 +1,69 @@
 //* Ternary operator with values
-const x = 16
-const isXNegative = x >= 0 ? 'no' : 'yes'
+const x = 16;
+const isXNegative = x >= 0 ? 'no' : 'yes';
 
-/*
-// class Grill {
-//   startGas() {}
-//   stopGas() {}
-// }
-// class Oven {
-//   setTemperature(degrees: number) {}
-// }
+class Grill {
+  startGas() {}
+  stopGas() {}
+}
+class Oven {
+  setTemperature(degrees: number) {}
+}
 
-// type CookingDevice<T> = T extends 'grill' ? Grill : Oven
+type CookingDevice<T> = T extends 'grill' ? Grill : Oven;
 
-// let device1: CookingDevice<'grill'>
-// //   ^?
-// let device2: CookingDevice<'oven'>
-// //   ^?
+let device1: CookingDevice<'grill'>;
+//   ^Grill
+let device2: CookingDevice<'oven'>;
+//   ^Oven
 
 //* Expressing Conditions
-/*
-// const one = 1
-// const two = 2
-// const ten = 10
 
-// type IsLowNumber<T> = T extends 1 | 2 ? true : false
-// type TestOne = IsLowNumber<1>
-// type TestTwo = IsLowNumber<2>
-// type TestTen = IsLowNumber<10>
-// type TestTenWithTwo = IsLowNumber<10 | 2>
+const one = 1;
+const two = 2;
+const ten = 10;
+
+type IsLowNumber<T> = T extends 1 | 2 ? true : false;
+type TestOne = IsLowNumber<1>; // = true
+type TestTwo = IsLowNumber<2>; // = true
+type TestTen = IsLowNumber<10>; // = false
+type TestTenWithTwo = IsLowNumber<10 | 2>; // = boolean <false | true>
 
 //* Extract<T, U>
-/*
-// type FavoriteColors =
-//   | 'dark sienna'
-//   | 'van dyke brown'
-//   | 'yellow ochre'
-//   | 'sap green'
-//   | 'titanium white'
-//   | 'phthalo green'
-//   | 'prussian blue'
-//   | 'cadium yellow'
-//   | [number, number, number]
-//   | { red: number; green: number; blue: number }
 
-// type StringColors = Extract<FavoriteColors, string>
-// //    ^?
-// type ObjectColors = Extract<FavoriteColors, { red: number }>
-// //    ^?
-// type TupleColors =
-//   //     ^?
-//   Extract<FavoriteColors, [number, number, number]>
+type FavoriteColors =
+  | 'dark sienna'
+  | 'van dyke brown'
+  | 'yellow ochre'
+  | 'sap green'
+  | 'titanium white'
+  | 'phthalo green'
+  | 'prussian blue'
+  | 'cadium yellow'
+  | [number, number, number]
+  | { red: number; green: number; blue: number };
+
+type StringColors = Extract<FavoriteColors, string>;
+//    ^only strings
+type ObjectColors = Extract<FavoriteColors, { red: number }>;
+//    ^only the one object
+type TupleColors = Extract<FavoriteColors, [number, number, number]>;
+//    ^only the tuple
 
 //* Exclude<T, U>
-/*
-// type NonStringColors = Exclude<FavoriteColors, string>
+
+type NonStringColors = Exclude<FavoriteColors, string>;
+//    ^only tuple and object
 
 //* How do these work?
-/*
-// type _Exclude<T, U> = T extends U ? never : T
-// type _Extract<T, U> = T extends U ? T : never
 
-// type OneNever = 1 | never
+type _Exclude<T, U> = T extends U ? never : T;
+type _Extract<T, U> = T extends U ? T : never;
+
+type OneNever = 1 | never;
 
 /**/
 
 console.log('all done.');
 
-export default {}
+export default {};
